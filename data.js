30e28fecce653023dfba1c2fb6150803fa4d0445
Temitope3665 <aroyewontope@gmail.com>
2021-10-14 18:13:42 +0100

    updated home page and quiz section functionality

---
+++ data.js
@@ -0,0 +1,102 @@
+export const dataArray = [
+    {
+        question: 'What is HTML', 
+        answers: {
+            a: 'HyperText Markup Language',
+            b: 'HyperText Makeup Link',
+            c: 'HorrorTechnology Markup Language',
+            d: 'Hyper Textlink Markup Language '
+        },
+        correctAnswer: 'a'
+    }, 
+    {
+        question: 'What is the meaning of UI UX Design ?', 
+        answers: {
+            a: 'User Interface and Use Experience',
+            b: 'User Introface and User Experience',
+            c: 'User Interface and User Experience',
+            d: 'User Interfacing and User Experience'
+        },
+        correctAnswer: 'c'
+    },
+    {
+        question: 'What is Logo', 
+        answers: {
+            a: 'HyperText Markup Language',
+            b: 'HyperText Makeup Link',
+            c: 'HorrorTechnology Markup Language',
+            d: 'Hyper Textlink Markup Language '
+        },
+        correctAnswer: 'a'
+    }, 
+    {
+        question: 'What is the meaning of UI UX Design ?', 
+        answers: {
+            a: 'User Interface and Use Experience',
+            b: 'User Introface and User Experience',
+            c: 'User Interface and User Experience',
+            d: 'User Interfacing and User Experience'
+        },
+        correctAnswer: 'c'
+    },
+    {
+        question: 'What is HTML', 
+        answers: {
+            a: 'HyperText Markup Language',
+            b: 'HyperText Makeup Link',
+            c: 'HorrorTechnology Markup Language',
+            d: 'Hyper Textlink Markup Language '
+        },
+        correctAnswer: 'a'
+    }, 
+    {
+        question: 'What is the meaning of UI UX Design ?', 
+        answers: {
+            a: 'User Interface and Use Experience',
+            b: 'User Introface and User Experience',
+            c: 'User Interface and User Experience',
+            d: 'User Interfacing and User Experience'
+        },
+        correctAnswer: 'c'
+    },
+    {
+        question: 'What is HTML', 
+        answers: {
+            a: 'HyperText Markup Language',
+            b: 'HyperText Makeup Link',
+            c: 'HorrorTechnology Markup Language',
+            d: 'Hyper Textlink Markup Language '
+        },
+        correctAnswer: 'a'
+    }, 
+    {
+        question: 'What is the meaning of UI UX Design ?', 
+        answers: {
+            a: 'User Interface and Use Experience',
+            b: 'User Introface and User Experience',
+            c: 'User Interface and User Experience',
+            d: 'User Interfacing and User Experience'
+        },
+        correctAnswer: 'c'
+    },
+    {
+        question: 'What is HTML', 
+        answers: {
+            a: 'HyperText Markup Language',
+            b: 'HyperText Makeup Link',
+            c: 'HorrorTechnology Markup Language',
+            d: 'Hyper Textlink Markup Language '
+        },
+        correctAnswer: 'a'
+    }, 
+    {
+        question: 'What is the meaning of UI UX Design ?', 
+        answers: {
+            a: 'User Interface and Use Experience',
+            b: 'User Introface and User Experience',
+            c: 'User Interface and User Experience',
+            d: 'User Interfacing and User Experience'
+        },
+        correctAnswer: 'c'
+    }
+]
---
+++ quiz.js
@@ -0,0 +1,70 @@
+import { dataArray } from "./data.js";
+
+const quizQuestion = document.querySelector('.quiz-question')
+const quizOptions = document.querySelector('.quiz-options')
+const questionNumbers = document.querySelector('.question-numbers')
+const prevBtn = document.querySelector('.prev-btn')
+const nextBtn = document.querySelector('.next-btn')
+const options = document.querySelectorAll('.option-wrapper')
+const answers = document.querySelectorAll('.answer-wrapper')
+
+let currentQuestion = dataArray[0];
+
+const displayQuestion = () => {
+    quizQuestion.innerHTML = currentQuestion.question
+    let displayOption = ''
+    // looping through the answer object of data.js
+    for(let option in currentQuestion.answers) {
+        displayOption += `<li class="d-flex align-items-center">
+        <p class="option-wrapper text-uppercase bg_gay rounded-circle">${option}</p>
+        <p class="answer-wrapper m-2">${currentQuestion.answers[option]}</p>
+        </li>`
+    }
+    quizOptions.innerHTML = displayOption
+}
+
+displayQuestion()
+
+const changeQuestion = (e) => {
+    // closest a-tag to click
+    const number = e.target.closest('a').innerHTML
+    currentQuestion = dataArray[number -1]
+    displayQuestion()
+}
+
+
+const changeToPrev = () => {
+    let currentIndex = dataArray.indexOf(currentQuestion)
+    if (currentIndex <= 0) return
+    currentQuestion = dataArray[currentIndex-1]
+    displayQuestion()
+}
+
+const changeToNext = () => {
+    let currentIndex = dataArray.indexOf(currentQuestion)
+    if (currentIndex === 9) return;
+    currentQuestion = dataArray[currentIndex + 1]
+    displayQuestion()
+}
+
+const selectAnswer = (e) => {
+    const selectedAnswer = e.target.closest('li')
+    options.forEach((option) => {
+        console.log(option)
+    })
+    console.log(options)
+    selectedAnswer.children[0].classList.add('selected-round')
+    selectedAnswer.children[1].classList.add('selected-text')
+}
+
+// to prev question
+prevBtn.addEventListener('click', changeToPrev)
+
+// to next question
+nextBtn.addEventListener('click', changeToNext)
+
+// to question numbers
+questionNumbers.addEventListener('click', changeQuestion)
+
+// options
+quizOptions.addEventListener('click', selectAnswer)
