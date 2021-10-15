4f3168ec7a3a9dbcd544bdb8bc4e01a93deaa83a
Temitope3665 <aroyewontope@gmail.com>
2021-10-15 15:29:09 +0100

    updated

--- javascript/util.js
+++ javascript/util.js
@@ -1,9 +1,5 @@
 export const auth = () => {
     let userToken = localStorage.getItem('Quizzy Login Token')
-    // console.log()
-    // if (location.pathname == '/' && userToken) {
-    //     return
-    // }
     if (!userToken ) {
         return location.href = '/'
     }
